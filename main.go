1f0b0d0f7ae0a67a3be7f2d8ba6c8d393dc205ee
JettCanavarro <98285768+JettCanavarro@users.noreply.github.com>
2023-04-20 09:08:50 -0400

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 type Server struct{}
 
-func (s *Serverasedfasdfasdf) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
    w.WriteHeader(http.StatusOK)
    w.Header().Set("Content-Type", "application/json")
    w.Write([]byte(`{"message": "Hello Golden Rams SP2023"}`))
